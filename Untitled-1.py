9119e724c33aeaf53e0cbbb79fb98cce50769ed3
Inaki-Gongora <i.gongora@alumno.etec.um.edu.ar>
2022-06-14 11:18:23 -0300

    Segunda Versión

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,7 @@
-print ("Hola mundo")
-lista = [1,2,3]
+def sumar (parametroCero,parametroUno,parametroDos):
+    i=parametroCero
+    for i in i:
+        suma=parametroUno+parametroDos
+        print (suma)
+
+sumar ((int(input("veces: "))),(int(input("veces: "))),(int(input("veces: "))))
